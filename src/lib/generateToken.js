012dd997a70d4537465e50b3b741892596ca644e
Your Name <you@example.com>
2025-09-04 13:28:55 +0000

    fixed login

--- src/lib/generateToken.js
+++ src/lib/generateToken.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "1d",
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: true,
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none", // allows cross-site cookies
+    secure: process.env.NODE_ENV !== "development", // secure only in prod
   });
 
   return token;
